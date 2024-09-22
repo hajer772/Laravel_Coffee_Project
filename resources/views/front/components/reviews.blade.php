986e1f608f9bd57b29e98a7b57c2370d91c49b23
fsd5802 <fsd5802@gmail.com>
2024-09-22 16:16:43 +0300

    After Edit review

--- resources/views/front/components/reviews.blade.php
+++ resources/views/front/components/reviews.blade.php
@@ -1,21 +1,24 @@
 <!-- Start Reviews Section -->
-<section class="reviews" id="reviews">
+<section class="reviews" id="reviews" dir="{{ App::isLocale('ar') ? 'ltr' : 'ltr' }}">
     <div class="container">
         <div class="row align-items-center">
             <!-- Image -->
-            <div class="col-12 col-md-6 wow  order-2 order-md-1 pt-5 pt-md-0 {{(App::isLocale('ar') ? 'fadeInRight' : 'fadeInLeft')}}" data-wow-duration="1s" data-wow-delay=".5s">
+            <div class="col-12 col-md-6 wow  order-2 order-md-1 pt-5 pt-md-0 {{ App::isLocale('ar') ? 'fadeInRight' : 'fadeInLeft' }}"
+                data-wow-duration="1s" data-wow-delay=".5s">
                 <div class="reviews-img">
-                    <img src=" {{ asset( 'front/images/reviews.jpg'  ) }}" alt="Testimonial">
+                    <img src=" {{ asset('front/images/reviews.jpg') }}" alt="Testimonial">
                 </div>
             </div>
             <!-- Content -->
-            <div class="col-12 col-md-6 wow  order-1 order-md-2 {{(App::isLocale('ar') ? 'fadeInRight' : 'fadeInLeft')}}" data-wow-duration="1s" data-wow-delay=".5s">
+            <div class="col-12 col-md-6 wow  order-1 order-md-2 {{ App::isLocale('ar') ? 'fadeInRight' : 'fadeInLeft' }}"
+                data-wow-duration="1s" data-wow-delay=".5s">
                 <div class="heading-area text-center">
-                    <div class="mb-4"><img src=" {{ asset( 'front/images/gallery-border.png'  ) }}" alt="About-line"></div>
-                    <h2 class="title main-font text-main my-4">{{ __("words.review-title") }}</h2>
+                    <div class="mb-4"><img src=" {{ asset('front/images/gallery-border.png') }}" alt="About-line">
+                    </div>
+                    <h2 class="title main-font text-main my-4">{{ __('words.review-title') }}</h2>
                 </div>
                 <div class="testimonial-carousel">
-                    <div class="testimonial-box owl-carousel owl-theme">
+                    <div class="testimonial-box owl-carousel owl-theme ">
                         @foreach ($testimonials as $testimonial)
                             <!-- Item-1 -->
                             <div class="item text-center animate-fade">
--- resources/views/front/layouts/scripts.blade.php
+++ resources/views/front/layouts/scripts.blade.php
@@ -21,29 +21,25 @@
 <!-- CUSTOM JS -->
 <script src=" {{ asset('front/js/swiper.min.js') }}"></script>
 
-
-    <!-- javascript -->
-    <script type="text/javascript">
-        function search() {
+@if (app()->getLocale() == 'ar')
+    <script src=" {{ asset('front/js/owl.carousel.min.ar.js') }}"></script>
+@else
+    <script src=" {{ asset('front/js/owl.carousel.min.js') }}"></script>
+@endif
+<!-- javascript -->
+<script type="text/javascript">
+    function search() {
         let filter = document.getElementById('find').value.toUpperCase();
         let item = document.querySelectorAll('.product');
         let l = document.getElementsByTagName('h3');
-        for(var i = 0;i<=l.length;i++){
-        let a=item[i].getElementsByTagName('h3')[0];
-        let value=a.innerHTML || a.innerText || a.textContent;
-        if(value.toUpperCase().indexOf(filter) > -1) {
-        item[i].style.display="";
+        for (var i = 0; i <= l.length; i++) {
+            let a = item[i].getElementsByTagName('h3')[0];
+            let value = a.innerHTML || a.innerText || a.textContent;
+            if (value.toUpperCase().indexOf(filter) > -1) {
+                item[i].style.display = "";
+            } else {
+                item[i].style.display = "none";
+            }
         }
-        else
-        {
-        item[i].style.display="none";
-        }
-        }
-        }
-        </script>
-
-
-
-
-
-
+    }
+</script>
